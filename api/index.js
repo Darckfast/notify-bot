9f5e89a8d81d1adbab68c076c5de08e66a78e0e5
V <victorcosta37@gmail.com>
2024-11-13 03:16:05 -0300

    fist commit

---
+++ api/index.js
@@ -0,0 +1,16 @@
+async function run() {
+    const res = await fetch("https://discord.com/api/webhooks/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            content: `Hey everyone! leez just posted on her main! Go check it out!
+            https://www.instagram.com/p/DCM3K69TJ39/`,
+        })
+    })
+
+    console.log(res)
+}
+
+run()
