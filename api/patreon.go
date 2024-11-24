43f2bcff046c178fd00e15a099b5ed0459aabb2e
V <victorcosta37@gmail.com>
2024-11-24 06:34:42 -0300

    chore: updated lib

--- api/patreon.go
+++ api/patreon.go
@@ -107,10 +107,10 @@ func ValidatePayloadSignature(signature string, payload []byte) bool {
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	ctx, wg := multilogger.SetupContext(&multilogger.SetupOps{
-		Request:     r,
-		BaselimeApiKey:      os.Getenv("BASELIME_API_KEY"),
-		AxiomApiKey:      os.Getenv("AXIOM_API_KEY"),
-		ServiceName: os.Getenv("VERCEL_GIT_REPO_SLUG"),
+		Request:        r,
+		BaselimeApiKey: os.Getenv("BASELIME_API_KEY"),
+		AxiomApiKey:    os.Getenv("AXIOM_API_KEY"),
+		ServiceName:    os.Getenv("VERCEL_GIT_REPO_SLUG"),
 	})
 
 	defer func() {
